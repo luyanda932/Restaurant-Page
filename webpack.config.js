1e3dc79f683b7076f53f23b5572e2f952b87d9f8
Luyanda Masina <luyandamasina14@gmail.com>
2026-06-22 12:56:14 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ export default {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-    publicPath: "/Restaurant-Page",
+    publicPath: "/Restaurant-Page/",
   },
 
   plugins: [
